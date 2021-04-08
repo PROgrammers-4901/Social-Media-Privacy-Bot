0baef18ab0e598f0c3238cf13b26308cfa81ea88
jon wright <jonathonwright@my.unt.edu>
2021-04-07 21:14:20 -0700

    strugglin

---
+++ aI_Model_Resources/createModel.py
@@ -0,0 +1,119 @@
+from preprocess import *
+from IPython.display import display
+import pandas as pd
+import sklearn
+import time
+import pickle
+from sklearn import preprocessing
+from sklearn import naive_bayes, svm
+from sklearn.model_selection import GridSearchCV,train_test_split
+from sklearn.metrics import accuracy_score,classification_report
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+
+def link_to_bool(temp):
+    if not temp:
+        return False
+    else:
+        return True
+
+def best_parameters_test(parameters,train_tf,test_tf,y_train,y_test,model_choice):
+
+    scores = ['precision', 'recall']
+
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = GridSearchCV(
+            model_choice, parameters, scoring='%s_macro' % score
+        )
+        clf.fit(train_tf, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(test_tf)
+        print(classification_report(y_true, y_pred))
+        print()
+
+def predict_tweet(tweet_list):
+    pickle_off = open("naive.pkl","rb")
+    model_1 = pickle.load(pickle_off)
+    processed_tweets = preprocess(tweet_list)
+    df = pd.DataFrame(processed_tweets, columns=['text','links','emojis','labels'])
+    x = [item for item in df['text']]
+    # vector = CountVectorizer()
+    # vector.fit(x)
+    # vector = CountVectorizer(vocabulary=vector.vocabulary_)
+    # x_tweets = vector.transform(x)
+    print(model_1.predict(x))
+
+def main():
+    tweet_data = preprocess()
+
+    encoder = preprocessing.LabelEncoder()
+    df = pd.DataFrame(tweet_data, columns=['text','links','emojis','labels'])
+    print(df.head())
+
+    x_train, x_test, y_train, y_test = train_test_split(df['text'],df['labels'],test_size=0.2)
+
+    encoder = preprocessing.LabelEncoder()
+
+    y_train_e = encoder.fit_transform(y_train)
+    y_test_e = encoder.transform(y_test)
+
+    #models = pd.DataFrame(columns=['models','model_object','score'])
+      
+    #      
+    # Term frequency inverse document frequency: convert text to numerical format 
+    # other ways: count vectorizering and ngrams
+    tf_vect = TfidfVectorizer()
+    tf_vect.fit(df['text'])
+
+    train_tf = tf_vect.transform(x_train)
+    test_tf = tf_vect.transform(x_test)
+
+    # NAIVE BAYES
+    Naive = naive_bayes.MultinomialNB()
+    Naive.fit(train_tf,y_train)# predict the labels on validation dataset
+    predictions_NB = Naive.predict(test_tf)# Use accuracy_score function to get the accuracy
+    print("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, y_test)*100)
+    print(x_test.head())
+    print(predictions_NB[0])
+
+    # Classifier - Algorithm - SVM
+    # fit the training dataset on the classifier
+    SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+    SVM.fit(train_tf,y_train)# predict the labels on validation dataset
+    predictions_SVM = SVM.predict(test_tf)# Use accuracy_score function to get the accuracy
+    print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, y_test)*100)
+    print(predictions_SVM[0])
+
+    #SVM gridsearch
+    parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],'C': [1, 10, 100, 1000]}, {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+    best_parameters_test(parameters,train_tf,test_tf,y_train,y_test,svm.SVC())
+
+    #Naive Bayes gridsearch
+    parameters = {}
+    best_parameters_test(parameters,train_tf,test_tf,y_train,y_test,naive_bayes.MultinomialNB())
+
+    pickle.dump(Naive, open( "naive.pkl", "wb" ))
+    pickle.dump(SVM, open( "SVM.pkl", "wb" ))
+
+if __name__ == "__main__":
+    main()
---
+++ aI_Model_Resources/get_tweet.py
@@ -0,0 +1,79 @@
+import requests
+import os
+import json
+
+from createModel import predict_tweet
+
+def getTweet(tweet_id):
+    BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
+
+    headers = {
+        'Authorization': f"Bearer {BEARER_TOKEN}",
+    }
+    
+    return requests.get(f'https://api.twitter.com/2/tweets/{tweet_id}', headers=headers)
+
+
+def getUser(username):
+    BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
+
+    headers = {
+        'Authorization': f"Bearer {BEARER_TOKEN}",
+    }
+    
+    user_data = requests.get(f'https://api.twitter.com/2/users/by/username/{username}', headers=headers)
+
+    user_data = json.loads(user_data.text)
+    user_id = user_data['data']['id']
+
+    params = {
+        "max_results": 100
+    }
+
+    return requests.get(f'https://api.twitter.com/2/users/{user_id}/tweets', params=params, headers=headers)
+
+def getHashtag(tag):
+    BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
+    tweets = []
+    headers = {
+        'Authorization': f"Bearer {BEARER_TOKEN}",
+    }
+    
+    params = {
+        'query': f"#{tag}",
+        'max_results': 100
+    }
+    
+    x = requests.get(f'https://api.twitter.com/2/tweets/search/recent', params=params, headers=headers)
+    x = json.loads(x.text)
+    # tweets.append(x)
+    for item in x['data']:
+        tweets.append(item['text'])
+    if 'meta' in x:
+        if 'next_token' in x['meta']:
+            next_token = x['meta']['next_token']
+        else:
+            truthy = False
+    else:
+        truthy = False 
+    while (truthy):
+        if (len(tweets) > 4):
+            truthy = False
+        params.update({'next_token': next_token})
+        x = requests.get(f'https://api.twitter.com/2/tweets/search/recent', params=params, headers=headers)
+        x = json.loads(x.text)
+        for item in x['data']:
+            tweets.append(item['text'])
+        if 'meta' in x:
+            if 'next_token' in x['meta']:
+                next_token = x['meta']['next_token']
+            else:
+                truthy = False
+        else:
+            truthy = False
+    
+    predict_tweet(tweet_list=tweets)
+    
+    return tweets
+
+getHashtag('stopDMCA')
