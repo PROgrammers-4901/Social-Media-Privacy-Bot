15e9b5e076c7dda5ececf3c92d2922495651eb18
Marcus Summers <MarcusSummers@my.unt.edu>
2021-04-10 23:52:12 -0500

    added outline for list of lists conversion from GET request text

--- AI/app.py
+++ AI/app.py
@@ -17,3 +17,9 @@ def tweet_by_user(username):
 @app.route('/hashtag/<tag>', methods=['GET'])
 def tweet_by_hashtag(tag):
     return json.dumps(getHashtag(tag),indent=4)
+
+# The function to convert returns from above to list of lists format
+	# STEPS
+		# the list of lists looks like list[[item2, item2]] (i.e., exactly 2 items within each item)
+		# parse the text for list of list components, appending a list[i][1] and list[i][2] for each
+		# return the list of lists for processing
